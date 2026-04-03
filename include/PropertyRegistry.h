dc9a39edf4f605caf8c2b25d0bd06b4956c337ea
Сергей Родин <srodin2011@gmail.com>
2026-04-03 13:31:54 +0300

    1.Решение проблемы с неуникальностью свойств по id в разных ветках иерархии. Добавлен второй ключ для получения метаданных - сам контрол.

--- include/PropertyRegistry.h
+++ include/PropertyRegistry.h
@@ -10,9 +10,16 @@
 
 namespace PluginGUI
 {
+    /// <summary>
+    /// Ведение реестра метаданных - дополнительных характеристик свойств,
+    /// уточняющих работу с ними.
+    /// </summary>
     class PropertyRegistry
     {
     public:
+        using TypeId = const std::type_info*;  // RTTI ключ
+        using Key = std::pair<TypeId, Id>;
+
         // lazy‑инициализируемый единый экземпляр
         static PropertyRegistry& Instance();
 
@@ -24,12 +31,22 @@ namespace PluginGUI
         };
 
         // Чтение по id (с автоматической инициализацией при первом вызове)
-        std::optional<PropertyMetadata> Get(int id);
+        std::optional<PropertyRegistry::PropertyMetadata> Get(const Control* pControl, int id);
 
         // Внутреннее добавление (для регистрационных функций)
-        void Register(Id id, const PropertyMetadata& meta);
+        void Register(TypeId type, Id id, const PropertyMetadata& meta);
 
     private:
+        struct KeyHash
+        {
+            size_t operator()(const Key& k) const noexcept
+            {
+                size_t h1 = std::hash<std::string_view>{}(k.first->name());
+                size_t h2 = std::hash<Id>{}(k.second);
+                return h1 ^ (h2 << 1);
+            }
+        };
+
         // Конструктор / деструктор — приватные, чтобы не создавать новые экземпляры
         PropertyRegistry() = default;
         ~PropertyRegistry() = default;
@@ -38,10 +55,11 @@ namespace PluginGUI
         void InitializeIfNecessary();
 
         mutable bool m_initialized = false;
-        mutable std::unordered_map<int, PropertyMetadata> m_registry;
+        mutable std::unordered_map<Key, PropertyMetadata, KeyHash> m_registry;
 
         // Друзья, чтобы могли использовать Register извне
         friend void RegisterControlProperties(PropertyRegistry& reg);
         friend void RegisterButtonControlProperties(PropertyRegistry& reg);
+        friend void RegisterButtonImageProperties(PropertyRegistry& reg);
     };
 }
